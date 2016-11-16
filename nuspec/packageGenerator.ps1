1d3bb1f05266cda185c870e8442dc9cf5d74347c
MACK Mathieu <mathieu.mack@LIVE.FR>
2016-11-16 22:16:20 +0100

    Hum ...

--- nuspec/packageGenerator.ps1
+++ nuspec/packageGenerator.ps1
@@ -1,7 +1,7 @@
-$location  = $env::APPVEYOR_BUILD_FOLDER
+$location  = $env:APPVEYOR_BUILD_FOLDER
 
 "Packaging to nuget..."
-	
+"Build folder : " + $location
 
 $strPath = $location + '\MvvX.CouchBaseLite\MvvX.Plugins.CouchBaseLite\bin\Release\MvvX.Plugins.CouchBaseLite.dll'
 
@@ -33,7 +33,7 @@ Set-Content $nuSpecFile
 
 .\NuGet pack MvvX.Plugins.CouchBaseLite.SQLCipher.nuspec
 
-#$apiKey = Read-Host 'Please set apiKey to publish to nuGet :'
+$apiKey = $env:NuGetApiKey
 	
 #.\NuGet push MvvX.Plugins.CouchBaseLite.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
 #.\NuGet push MvvX.Plugins.CouchBaseLite.ForestDB.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
