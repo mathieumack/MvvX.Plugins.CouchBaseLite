e567f70b87e5fabae1abe4fb5aa3f482a77c1319
ArnaudMarcille <arnaud.marcille@hotmail.fr>
2017-05-17 17:57:14 +0200

    Fix IOS nuspec package (#5)
    Fix issues #4

---
+++ nuspec/BootstrapContentTouch/CouchBaseLitePluginBootstrap.cs.pp
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+ namespace $rootnamespace$.Bootstrap
+ {
+    public class CouchBaseLitePluginBootstrap
+        : MvxLoaderPluginBootstrapAction<MvvX.Plugins.CouchBaseLite.PluginLoader, MvvX.Plugins.CouchBaseLite.Platform.Plugin>
+    {
+    }
+ }
