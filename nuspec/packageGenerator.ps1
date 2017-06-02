345ab071bc03bf5d0fefa9eeeeef0ea9d440fa49
MACK Mathieu <mathieu.mack@econocom.com>
2017-06-03 00:03:51 +0200

    Update packaging script.

--- nuspec/packageGenerator.ps1
+++ nuspec/packageGenerator.ps1
@@ -1,3 +1,58 @@
+function PublishTweets {
+    param (
+        [string]$message,
+        [string]$oauth_consumer_key,
+        [string]$oauth_consumer_secret,
+        [string]$oauth_token,
+        [string]$oauth_token_secret
+    )
+    
+    [Reflection.Assembly]::LoadWithPartialName("System.Security")  
+    [Reflection.Assembly]::LoadWithPartialName("System.Net")  
+
+    $status = [System.Uri]::EscapeDataString($message);
+
+    $oauth_nonce = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes([System.DateTime]::Now.Ticks.ToString()));  
+    $ts = [System.DateTime]::UtcNow - [System.DateTime]::ParseExact("01/01/1970", "dd/MM/yyyy", $null).ToUniversalTime();  
+    $oauth_timestamp = [System.Convert]::ToInt64($ts.TotalSeconds).ToString();  
+
+    $signature = "POST&";  
+    $signature += [System.Uri]::EscapeDataString("https://api.twitter.com/1.1/statuses/update.json") + "&";  
+    $signature += [System.Uri]::EscapeDataString("oauth_consumer_key=" + $oauth_consumer_key + "&");  
+    $signature += [System.Uri]::EscapeDataString("oauth_nonce=" + $oauth_nonce + "&");   
+    $signature += [System.Uri]::EscapeDataString("oauth_signature_method=HMAC-SHA1&");  
+    $signature += [System.Uri]::EscapeDataString("oauth_timestamp=" + $oauth_timestamp + "&");  
+    $signature += [System.Uri]::EscapeDataString("oauth_token=" + $oauth_token + "&");  
+    $signature += [System.Uri]::EscapeDataString("oauth_version=1.0&");  
+    $signature += [System.Uri]::EscapeDataString("status=" + $status);  
+
+    $signature_key = [System.Uri]::EscapeDataString($oauth_consumer_secret) + "&" + [System.Uri]::EscapeDataString($oauth_token_secret);  
+
+    $hmacsha1 = new-object System.Security.Cryptography.HMACSHA1;  
+    $hmacsha1.Key = [System.Text.Encoding]::ASCII.GetBytes($signature_key);  
+    $oauth_signature = [System.Convert]::ToBase64String($hmacsha1.ComputeHash([System.Text.Encoding]::ASCII.GetBytes($signature)));  
+
+    $oauth_authorization = 'OAuth ';  
+    $oauth_authorization += 'oauth_consumer_key="' + [System.Uri]::EscapeDataString($oauth_consumer_key) + '",';  
+    $oauth_authorization += 'oauth_nonce="' + [System.Uri]::EscapeDataString($oauth_nonce) + '",';  
+    $oauth_authorization += 'oauth_signature="' + [System.Uri]::EscapeDataString($oauth_signature) + '",';  
+    $oauth_authorization += 'oauth_signature_method="HMAC-SHA1",'  
+    $oauth_authorization += 'oauth_timestamp="' + [System.Uri]::EscapeDataString($oauth_timestamp) + '",'  
+    $oauth_authorization += 'oauth_token="' + [System.Uri]::EscapeDataString($oauth_token) + '",';  
+    $oauth_authorization += 'oauth_version="1.0"';  
+
+    $post_body = [System.Text.Encoding]::ASCII.GetBytes("status=" + $status);   
+    [System.Net.HttpWebRequest] $request = [System.Net.WebRequest]::Create("https://api.twitter.com/1.1/statuses/update.json");  
+    $request.Method = "POST";  
+    $request.Headers.Add("Authorization", $oauth_authorization);  
+    $request.ContentType = "application/x-www-form-urlencoded";  
+    $body = $request.GetRequestStream();  
+    $body.write($post_body, 0, $post_body.length);  
+    $body.flush();  
+    $body.close();  
+    $response = $request.GetResponse();
+}
+
 write-host "**************************" -foreground "Cyan"
 write-host "*   Packaging to nuget   *" -foreground "Cyan"
 write-host "**************************" -foreground "Cyan"
@@ -49,3 +104,13 @@ write-host "Publish nuget packages" -foreground "Green"
 .\NuGet push MvvX.Plugins.CouchBaseLite.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
 .\NuGet push MvvX.Plugins.CouchBaseLite.ForestDB.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
 .\NuGet push MvvX.Plugins.CouchBaseLite.SQLCipher.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
+
+$oauth_consumer_key = $env:oauth_consumer_key
+$oauth_consumer_secret = $env:oauth_consumer_secret
+$oauth_token = $env:oauth_token
+$oauth_token_secret = $env:oauth_token_secret
+$messageToPublish = "New #mvvmcross plugin version for #CouchBaseLite ! Version : $ProductVersion. https://www.nuget.org/packages/MvvX.Plugins.CouchBaseLite/"
+
+write-host "Send notification for new version" -foreground "Green"	
+PublishTweets -message $messageToPublish -oauth_consumer_key $oauth_consumer_key
+ -oauth_consumer_secret $oauth_consumer_secret -oauth_token $oauth_token -oauth_token_secret $oauth_token_secret
